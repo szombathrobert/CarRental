2f1b53424d17f2e0c0359d3aa60f283ab965e654
szombathrobert <szombathrobert4@gmail.com>
2023-09-19 21:48:04 +0200

    navbar and header hotfix

--- CarRentalPage/js/main.js
+++ CarRentalPage/js/main.js
@@ -1,13 +1,17 @@
-$('.toggle').click(function () {
-    "use strict";
-    $('nav ul').slideToggle();
-});
-
-
-
-$(window).resize(function () {
-    "use strict";
-    if ($(window).width() > 780) {
-        $('nav ul').removeAttr('style');
-    }
-});
+const body = document.querySelector("body");
+const navbar = document.querySelector(".exnav");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = ()=>{
+  navbar.classList.add("show");
+  menuBtn.classList.add("hide");
+  body.classList.add("disabled");
+}
+cancelBtn.onclick = ()=>{
+  body.classList.remove("disabled");
+  navbar.classList.remove("show");
+  menuBtn.classList.remove("hide");
+}
+window.onscroll = ()=>{
+  this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+}
